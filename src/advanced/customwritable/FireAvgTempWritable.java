6989372bc6c667126f0164eb79f4a87554f76ee6
Unknown <jpbarddal@terra.com.br>
2020-09-07 12:04:25 -0300

    FIleUtil

--- src/advanced/customwritable/FireAvgTempWritable.java
+++ src/advanced/customwritable/FireAvgTempWritable.java
@@ -6,17 +6,62 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
+
+// IntWritable, FloatWritable, Text -> Writable
+// Writable eh uma classe Serializavel
+// Ser serializavel significa que um objeto pode ser "binarizado" (0s e 1s)
+// e consequentemente, pode ser armazenado em disco ou transmitido pela rede
+
+
+// Java Bean
+// 1- Todos os atributos sao privados / protected
+// 2- Gets e sets para todos os atributos
+// 3- Construtor vazio (note que podemos ter outros construtores,
+//                      mas precisamos sempre do vazio)
+
 public class FireAvgTempWritable implements Writable {
 
+    // Atributos privados
+    private int n;
+    private float soma;
+
+    // Construtor vazio
+    public FireAvgTempWritable() {
+    }
+
+    public FireAvgTempWritable(int n, float soma) {
+        this.n = n;
+        this.soma = soma;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public float getSoma() {
+        return soma;
+    }
+
+    public void setSoma(float soma) {
+        this.soma = soma;
+    }
 
     @Override
     public void readFields(DataInput in) throws IOException {
-
+        // Seguindo a mesma ordem do write, isto eh, n e depois soma
+        n = Integer.parseInt(in.readUTF());
+        soma = Float.parseFloat(in.readUTF());
     }
 
     @Override
     public void write(DataOutput out) throws IOException {
-
+        // escrever n e soma, nesta ordem!
+        out.writeUTF(String.valueOf(n));
+        out.writeUTF(String.valueOf(soma));
     }
 
 }
--- src/basic/WordCount.java
+++ src/basic/WordCount.java
@@ -30,11 +30,42 @@ public class WordCount {
         Path output = new Path(files[1]);
 
         // criacao do job e seu nome
-        Job j = new Job(c, "wordcount-professor");
+        Job j = new Job(c, "wordcount-jean");
+
+        // Registro das classes
+        j.setJarByClass(WordCount.class); // Classe do main
+        j.setMapperClass(MapForWordCount.class); // Classe do map
+        j.setReducerClass(ReduceForWordCount.class); // Classe do reduce
+        j.setCombinerClass(CombinerForWordCount.class); // Classe do combiner!
+
+        // Definicao dos tipos de saida (reduce)
+        j.setOutputKeyClass(Text.class);
+        j.setOutputValueClass(IntWritable.class);
+
+        // cadastro dos arquivos de entrada e saida
+        FileInputFormat.addInputPath(j, input);
+        FileOutputFormat.setOutputPath(j, output);
 
         // lanca o job e aguarda sua execucao
         System.exit(j.waitForCompletion(true) ? 0 : 1);
     }
+
+
+    /**
+     * Classe de Map
+     * 4 tipos:
+     * - Tipo da chave de entrada
+     * - Tipo do valor da entrada
+     * - Tipo da chave de saida
+     * - Tipo da saida
+     */
+
+    /**
+     * Lembrete: LongWritable, IntWritable, Text, (FloatWritable, etc), sao wrappers.
+     * Wrappers permitem que o hadoop seja capaz de transferir os objetos pela rede e tambem salva-los em disco.
+     *
+     * Nota: nos podemos usar os Writables do Hadoop, ou criar os nossos proprios Writables. (vamos fazer isso na sequencia!)
+     */
 
     public static class MapForWordCount extends Mapper<LongWritable, Text, Text, IntWritable> {
 
@@ -42,17 +73,73 @@ public class WordCount {
         public void map(LongWritable key, Text value, Context con)
                 throws IOException, InterruptedException {
 
+            // lembrete: o map eh executado por bloco de arquivo
+            // contudo, nos vamos ver que na realidade, o map eh executado por LINHA do arquivo
+
+            // Minha linha esta em "value"
+            // Converter a linha em string
+            String linha = value.toString();
+
+            // Quebrando em palavras
+            String[] palavras = linha.split(" ");
+
+            // Loop -> gerar as tuplas no formato <chave = palavra, valor = ocorrencia = 1>
+            for(String p : palavras){
+                // criando chave de saida
+                Text chaveSaida = new Text(p);
+                // criando valor de saida
+                IntWritable valorSaida = new IntWritable(1);
+
+                // emitir a tupla (chave, valor)
+                con.write(chaveSaida, valorSaida);
+            }
 
         }
     }
 
+    /**
+     * Classe de Reduce
+     * 4 tipos:
+     * - Tipo da chave de entrada
+     * - Tipo do valor de entrada
+     * - Tipo da chave de saida
+     * - Tipo do valor de saida
+     */
+
     public static class ReduceForWordCount extends Reducer<Text, IntWritable, Text, IntWritable> {
 
         // Funcao de reduce
+        // A entrada do reduce sera (chave = palavra, lista de valores = lista de ocorrencias 1s)
         public void reduce(Text word, Iterable<IntWritable> values, Context con)
                 throws IOException, InterruptedException {
+            // Objetivo do reduce: fazer um loop para somar todas as ocorrencias de uma palavra
+            // E no final, emitir a palavra com a ocorrencia ja somada
+            int soma = 0;
+            for(IntWritable vlr : values){
+                soma += vlr.get(); // get retorna um int
+            }
 
+            // criando tupla com o resultado final (palavra, soma)
+            con.write(word, new IntWritable(soma));
+            // Lembrete: no reduce, o write escreve em arquivo!
         }
+    }
+
+    public static class CombinerForWordCount extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        // O combiner eh muito parecido com um reducer, ele eh executado por CHAVE apos a execucao de cada MAP
+        // O combiner eh executado por BLOCO, assim como o MAP
+        public void reduce(Text word, Iterable<IntWritable> valores, Context con)
+                throws IOException, InterruptedException{
+            // o combiner vai somar as ocorrencias das palavras naquele bloco
+            int soma = 0;
+            for(IntWritable vlr : valores){
+                soma += vlr.get();
+            }
+
+            con.write(word, new IntWritable(soma));
+        }
+
     }
 
 }
